ec15385342abdba55b9910e9db0d38cfb5728257
abu_112_bakr <abu.bakar.hasnath@g.bracu.ac.bd>
2026-01-14 12:19:03 +0600

    new 1

---
+++ l1_variables.java
@@ -0,0 +1,18 @@
+public class l1_variables {
+    public static void main(String[] args) {
+        // Variable declaration and initialization
+        int myNumber = 10;
+        String myString = "Hello, Java!";
+        double myDouble = 5.99;
+        boolean myBoolean = true;
+        int num1 = 15; 
+        int num2 = 25;
+
+        // Printing variables
+        System.out.println("Integer: " + myNumber);
+        System.out.println("String: " + myString);
+        System.out.println("Double: " + myDouble);
+        System.out.println("Boolean: " + myBoolean);
+        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2) );
+    }
+}
