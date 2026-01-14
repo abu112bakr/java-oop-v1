6782d8b50fbbf9cd9a84d1806fa3c860940e186d
abu_112_bakr <abu.bakar.hasnath@g.bracu.ac.bd>
2026-01-14 12:38:57 +0600

    l3 printing

---
+++ l2_data_type.java
@@ -0,0 +1,16 @@
+public class l2_data_type {
+
+    public static void main(String[] args) {
+    System.out.println("Primitive Data Types: Holds a value, specifies the siaze and type of a variable");
+    System.out.println("Non Primitive Data Types: Holds a reference to an object or location");
+    System.out.println("Data Types: byte(8-bit integer), short(-32,768 to 32,767), int(long), long, float, double, boolean(truefalse)");
+    System.out.println("Non Primitive Data Types: String, Arrays, Classes, Interface");
+    System.out.println("byte -128 to 127");
+    System.out.println("Short -32,768 to 32,767");
+    System.out.println("int -2,147,483,648 to 2,147,483,647");
+    System.out.println("long -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807");
+    System.out.println("float -1.4E-45 to 3.4E+38");
+    System.out.println("double -4.9E-324 to 1.8E+308");
+    System.out.println("boolean true or false");
+    }
+}
---
+++ l3_printing.java
@@ -0,0 +1,8 @@
+public class l3_printing {
+    public static void main(String[] args) {
+        System.out.println("we dont need an object or instance to call a static method, this is executed automatically");
+        System.out.println("System.out.println() - prints a line");
+        System.out.println("System.out.print() - prints without a newline");
+        System.out.printf("System.out.printf() - formatted printing");
+    }
+}
